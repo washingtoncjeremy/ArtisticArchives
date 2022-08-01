e50be0422067f9913cc97d48e02e47ab585c3bdb
Jeremy Washington <thebeluvdtrex@gmail.com>
2022-08-01 11:17:50 -0700

    added user pool info

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_rrsOVBzFl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3h4hjgjglvr3pefm9ve1vl3uvh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
